e6e7940185709f48ee460c71d2a190188cad7f3f
hannd15 <lejhernandez08@gmail.com>
2024-03-27 10:32:21 -0500

    Checkpoint xd

--- main.js
+++ main.js
@@ -1,85 +1,134 @@
 const form = document.getElementById('calc-form')
 const answerDiv = document.getElementById('answer')
-answerDiv.style.visibility='hidden'
+const maskSelect = document.getElementById('maskNumber')
+const selectCIDR = document.getElementById('CIDR')
+
+const TOTAL_OCTATES = 4
+const MAX_NET_BITS = 30
+const WHOLE_BYTE = 255
+let netOct
+let hostOct
+
+const Answer=(title, content)=>{
+    answerDiv.insertAdjacentHTML ('beforeend',
+        `<div class="answer-section">
+        <strong>${title}:</strong>
+        <br>
+        ${content}
+        </div>`
+    )
+}
 
 const ipCheck = (ip)=>{
+    const hostOct = TOTAL_OCTATES - netOct
     for (i of ip){
-        if (i<=255){
+        if (i<=WHOLE_BYTE){
             continue;
         } else {
-            alert('Recuerda que el valor máximo de cada campo es de 255')
             return 1;
         }
     }
+    return 0;
 }
 
-const snCheck = (n)=>{
-if (n > 64){
-    alert('No pueden haber más de 64 Subredes')
-    return 1;
+const cidrCheck =(ip,CIDR)=>{
+    switch (CIDR) {
+        case "A":
+            if (ip[0]<127){
+                netOct = 1
+                break
+            }else{
+                alert("Las redes tipo A no pueden tener ese número de red")
+                return 1
+            }
+        case "B":
+            if (ip[0]<191 && ip[0]>128){
+                netOct = 2
+                break
+            }else{
+                alert("Las redes tipo B no pueden tener ese número de red")
+                return 1
+            }
+        case "C":
+            if (ip[0]<223 && ip[0]>192){
+                netOct = 3
+                break
+            }else{
+                alert("Las redes tipo C no pueden tener ese número de red")
+                return 1
+            }
+    }
+    return 0
 }
+
+const updateMasks = (netBits)=>{
+    maskSelect.innerHTML=''
+    for (let i = netBits; i <= MAX_NET_BITS; i++) {
+        maskSelect.insertAdjacentHTML('beforeend',`<option value="${i}">${i}</option>`)
+    }
 }
+
+updateMasks(8)
+
+selectCIDR.addEventListener('change',(e)=>{
+    switch (e.target.value) {
+        case "A":
+            updateMasks(8)
+            break;
+        case "B":
+            updateMasks(16)
+            break;
+        case "C":
+            updateMasks(24)
+            break;
+        default:
+            break;
+    }
+})
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     answerDiv.innerHTML=''
-    const n = e.target.SNnumber.value
     const network = e.target.IPnumber.value.split('.')
-    let CIDR
-
-    if (network[0]<=127){
-        CIDR = 8
-    } else if (network[0]<=191){
-        CIDR = 16
-    } else if (network[0]<=223){
-        CIDR = 24
-    }
+    const CIDR = e.target.CIDR.value
+    const subNetMask = e.target.maskNumber.value
+    const netMask = []
 
     err = ipCheck(network)
-    err = snCheck(n)
+    err = cidrCheck(network,CIDR)
 
-    if (err) {
-        alert('Problema con los valores')
-        return;
-    } else {
+    if (!err){
+        netOct=Math.floor(subNetMask/8)
+        hostOct=TOTAL_OCTATES-netOct
+        const magicNumber = WHOLE_BYTE>>(subNetMask%8)
+        const extraBits = WHOLE_BYTE-magicNumber
+        for (i=0;i<netOct;i++){
+            netMask.push(WHOLE_BYTE)
+        }
+        for (i=0;i<hostOct;i++){
+            netMask.push(i===0?(extraBits):0)
+        }
+
         answerDiv.style.visibility='visible'
-        let pw = 0
-        let mask = 0
-        let h = 0
-        let nm = 0
-        let tSN
-
-        while (true) {
-            tSN = 2**pw
-            if ((tSN)<n){
-                pw++
-                mask += 2**(8-pw)
-                continue
-            } else {
-                h = (2**(8-pw))-2
-                nm = 256-mask
-                break;
+        Answer('Máscara de subred',netMask.join('.'))
+        
+        const netAddress = []
+        for(i=0;i<TOTAL_OCTATES;i++){
+            if (netMask[i] && netMask[i]===WHOLE_BYTE){
+                netAddress.push(network[i])
+            }else{
+                netAddress.push(0)
             }
         }
+        Answer('Dirección de subred',netAddress.join('.'))
 
-        answerDiv.innerHTML = "<h4>Posibles subredes:</h4>"
-
-        answerDiv.insertAdjacentHTML('afterbegin',`Máscara: 255.255.255.${mask}`)
-
-        console.log(network, pw, h, nm , mask);
-
-        const baseAddress = JSON.parse(JSON.stringify(network))
-
-        for (let i=0;i<tSN;i++){
-            baseAddress.pop()
-            const minAddress = JSON.parse(JSON.stringify(baseAddress))
-            minAddress.push((i*nm)+1)
-            const maxAddress = JSON.parse(JSON.stringify(baseAddress))
-            maxAddress.push(((i+1)*nm)-2)
-            baseAddress.push(i*nm)
-            answerDiv.insertAdjacentHTML('beforeend', `<p>${baseAddress}/${CIDR+pw} - Rango válido (${minAddress} - ${maxAddress})</p>`)
-            minAddress.pop()
-            maxAddress.pop()
+        const firstUsableAddress = []
+        const lastUsableAddress = []
+        for (i=0;i<netOct;i++){
+            firstUsableAddress.push(netAddress[i])
+            lastUsableAddress.push(netAddress[i])
         }
+
+        Answer('Rango de hosts usables',`${firstUsableAddress.join('.')} - ${lastUsableAddress.join('.')}`)
     }
 })
